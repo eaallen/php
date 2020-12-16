0ac7ca776291bf4b814c0bd8b94f8ecbd0bcf537
eaallen <elijah.a.allen@gmail.com>
2020-12-15 21:14:50 -0700

    own_mvc

---
+++ own_mvc/core/Router.php
@@ -0,0 +1,31 @@
+<?php
+
+// class for routing 
+
+class Router {
+    public function __construct()
+    {
+        
+    }
+
+    public static function test(){
+
+    }
+
+    public function get($address)
+    {
+        $uri = $this->httpsOrHttp();
+        header('Location: '.$uri."$address");
+        exit;
+    }
+
+    private function httpsOrHttp()
+    {
+        if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+            $uri = 'https://';
+        } else {
+            $uri = 'http://';
+        }
+        return $uri .= $_SERVER['HTTP_HOST'];
+    }
+}
---
+++ own_mvc/index.php
@@ -0,0 +1,5 @@
+<?php
+include './core/Router.php';
+// $app = new Application()
+$router = new Router();
+$router->get('/do-or-die');
