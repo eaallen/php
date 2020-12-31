22bb29b3586d24ceefc4c732596b8428ff4b254d
eaallen <elijah.a.allen@gmail.com>
2020-12-31 09:36:16 -0700

    .

--- demo/prac/embed.php
+++
@@ -1,5 +0,0 @@
-<?php echo "hi there";
-    $title = "Eli is the cream of the crop" 
-
-
-?>
--- demo/prac/mysql/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $sql_con = null;
-    // global $sql_con;
-    $sql_con = mysqli_connect('localhost','root', '', 'loginapp');
-        if(!$sql_con){
-            die("connection faild");
-        }
-?>
